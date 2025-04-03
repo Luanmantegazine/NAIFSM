cbadb2c54d968c180f970f9ad6fd1233000e332e
luanmantegazine <luanramos27@gmail.com>
2025-04-03 13:30:15 -0300

    new commit 10

--- main.py
+++ main.py
@@ -6,17 +6,35 @@ import os
 app = FastAPI()
 
 GITHUB_WEBHOOK_SECRET = os.environ.get("GITHUB_WEBHOOK_SECRET", "senha")
-def signature_valid(payload: bytes, signature_header: str)-> bool:
-    if not signature_header or "=" not in signature_header:
-        return False
-    sha_name, received_signature = signature_header.split("=")
-    if sha_name != "sha256":
-        return False
+def signature_valid(payload: bytes, signature_sha256: str, signature_sha1: str)-> bool:
+    if signature_sha256:
+        try:
+            sha_name, signature = signature_sha256.split("=")
+        except ValueError:
+            return False
 
-    mac = hmac.new(GITHUB_WEBHOOK_SECRET.encode(), msg=payload, digestmod=hashlib.sha256)
-    expected = mac.hexdigest()
+        if sha_name != "sha256":
+            return False
 
-    return hmac.compare_digest(expected, received_signature)
+        mac = hmac.new(GITHUB_WEBHOOK_SECRET.encode(), msg=payload, digestmod=hashlib.sha256)
+        if hmac.compare_digest(mac.hexdigest(), signature):
+            return True
+
+        # Fallback to SHA-1 if available
+    if signature_sha1:
+        try:
+            sha_name, signature = signature_sha1.split("=")
+        except ValueError:
+            return False
+
+        if sha_name != "sha1":
+            return False
+
+        mac = hmac.new(GITHUB_WEBHOOK_SECRET.encode(), msg=payload, digestmod=hashlib.sha1)
+        if hmac.compare_digest(mac.hexdigest(), signature):
+            return True
+
+    return False
 
 @app.get("/")
 def home():
@@ -29,9 +47,10 @@ async def webhook_handler(
     x_github_event: str = Header("ping")
 ):
     body = await request.body()
-    signature = request.headers.get("X-Hub-Signature-256")
+    signature256 = request.headers.get("X-Hub-Signature-256")
+    signature1 = request.headers.get("X-Hub-Signature-1")
 
-    if not signature_valid(body, signature):
+    if not signature_valid(body, signature256, signature1):
         raise HTTPException(status_code=401, detail="Invalid signature")
 
     data = await request.json()
