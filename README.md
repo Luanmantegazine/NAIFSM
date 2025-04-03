1188c0f1060d311c7161381fd29151f37fa93831
luanmantegazine <luanramos27@gmail.com>
2025-04-03 09:30:58 -0300

    uptade project

---
+++ README.md
@@ -0,0 +1,22 @@
+# github-webhook-fastapi
+
+## Tutorial para criar e testar um Webhook do GitHub com FastAPI
+
+---
+
+## Requisitos
+
+- Python 3.8+
+- Git
+- Ngrok (ou outro túnel local)
+- Conta no GitHub com um repositório de testes
+
+---
+
+## Passos para configurar o ambiente local
+
+### 1) Clone o repositório ou crie a estrutura
+
+```bash
+git clone https://github.com/luanmantegazine/github-webhook-fastapi.git
+cd github-webhook-fastapi
---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Request, Header
+from starlette.responses import JSONResponse
+import hmac
+import hashlib
+import os
+app = FastAPI()
+
+GITHUB_WEBHOOK_SECRET = os.environ.get("GITHUB_WEBHOOK_SECRET", "l12345")
+
+@app.get("/")
+def home():
+    return "GitHub Webhook is running!", 200
+
+
+@app.post("/webhook")
+async def webhook_handler(
+    request: Request,
+    x_hub_signature_256: str = Header(None),
+    x_github_event: str = Header("ping")
+):
+    body = request.body()
+    data = await request.json()
+
+    if x_github_event == "ping":
+        print("Received ping from GitHub!")
+    elif x_github_event == "push":
+        print("Received a push event with the following commits:")
+        for commit in data.get("commits", []):
+            print(" -", commit["message"])
+    else:
+        print(f"Received GitHub event: {x_github_event}")
+
+    # 5. Return a 200 OK response so GitHub knows we received it
+    return JSONResponse({"status": "ok"})
+
+
+def signature_valid(payload: bytes, signature_header: str)-> bool:
+    if not signature_header or "=" not in signature_header:
+        return False
+    sha_name, received_signature = signature_header.split("=")
+    if sha_name != "sha256":
+        return False
+
+    mac = hmac.new(GITHUB_WEBHOOK_SECRET.encode(), msg=payload, digestmod=hashlib.sha256)
+    expected = mac.hexdigest()
+
+    return hmac.compare_digest(expected, received_signature)
+
