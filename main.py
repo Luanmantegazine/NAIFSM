21e54d1a4a2f8e2971f50bd4d9357086164482c8
luanmantegazine <luanramos27@gmail.com>
2025-04-03 23:42:38 -0300

    new commit 14

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Request, Header, HTTPException
 import uvicorn
+from typing import Optional, Dict
 import hmac
 import hashlib
+import json
 import os
 app = FastAPI()
+
 
 @app.get("/")
 def home():
@@ -11,11 +14,18 @@ def home():
 
 
 @app.post("/webhook")
-async def webhook_handler(payload: dict):
-    if "after" in payload:
-        return {"message": "There was a push event"}
-    else:
-        raise HTTPException(status_code=400, detail='no payload')
+async def webhook_handler(
+        x_github_event: Optional[str] = Header(None)
+):
+    # Processar eventos do GitHub
+    if x_github_event == "ping":
+        return {"message": "Hi! This is a FastAPI webhook"}
+
+    if x_github_event != "push":
+        raise HTTPException(400, "Unsupported event type")
+
+    # Carregar payload e configurações
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
